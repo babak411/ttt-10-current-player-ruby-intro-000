055f2d7e45f85b44f939a0cd36ff8a7383053958
Babak Khodabandeh <babak.khodabandeh@gmail.com>
2020-04-02 19:19:47 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |spaces|
-    if spaces == "X" || "O"
+    if spaces == "X" || spaces == "O"
     counter += 1
   end
 end
